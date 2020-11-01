3b27e17ebdc99c875b51ec7b0a11c41c65dd7f5e
Andrew Smith <smith.andrew.l@gmail.com>
2020-10-31 19:30:25 -0500

    Moved the UI of the create-task component over.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AppSiteHeadingComponent } from './app-site-heading/app-site-heading.component';
 import { FrontendAppComponent } from './frontend-app/frontend-app.component';
 import { HomeComponent } from './home/home.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppSiteHeadingComponent,
     FrontendAppComponent,
-    HomeComponent
+    HomeComponent,
+    CreateTaskComponent
   ],
   imports: [
     BrowserModule,
