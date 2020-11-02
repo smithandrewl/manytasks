c9fcbfae1bd5d2fa032a53b7dc79074b441978e4
Andrew Smith <smith.andrew.l@gmail.com>
2020-11-01 19:59:43 -0600

    Fleshed out the List Tasks component.

--- frontend/src/components/listTasks.vue
+++ frontend/src/components/listTasks.vue
@@ -1,5 +1,35 @@
 <template>
-  <b>List Tasks</b>
+  <v-card>
+    <v-card-title>List Tasks</v-card-title>
+
+
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-card>
+              <v-card-title>Tasks</v-card-title>
+
+              <v-simple-table>
+                <thead>
+                  <tr>
+                    <th>UserId</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>task.UserId</td>
+                    <td>task.Title</td>
+                    <td>task.Description</td>
+                  </tr>
+                </tbody>
+              </v-simple-table>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+  </v-card>
 </template>
 
 <script lang="ts">
